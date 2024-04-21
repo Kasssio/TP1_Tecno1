93338ab8bece75f5e2427c1cdd8f400723087e97
LucasRehmann <101809208+LucasRehmann@users.noreply.github.com>
2024-04-20 23:47:03 -0300

    Mas tests

--- templates/distancia_binaria_tests.py
+++ templates/distancia_binaria_tests.py
@@ -26,7 +26,7 @@ class TestVecinosBinarios(unittest.TestCase):
     def test_aledaños_menores(self):
         self.assertEqual(aledaños_menores(127),[63, 95, 111, 119, 123, 125, 126])
         self.assertEqual(aledaños_menores(64),[])
-        self.assertEqual(aledaños_menores(0),[])
+        self.assertEqual(aledaños_menores(5),[1, 4])
         self.assertEqual(aledaños_menores(1),[])
         self.assertEqual(aledaños_menores(10),[2, 8])
         self.assertEqual(aledaños_menores(100),[36, 68, 96])
@@ -40,5 +40,7 @@ class TestVecinosBinarios(unittest.TestCase):
     def test_densidad_intervalo(self):
         self.assertEqual(densidad_intervalo(10,8,13),0.33333)
         self.assertEqual(densidad_intervalo(64,1,63),0.0)
-
+        self.assertEqual(densidad_intervalo(100,50,100),0.03922)
+        self.assertEqual(densidad_intervalo(50,20,80),0.09804)
+ 
 unittest.main()
